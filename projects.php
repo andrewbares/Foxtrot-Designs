424a2c2f9908e1ce95015683da3d8582aa272cd4
andrewbares <andrewbares@outlook.com>
2014-12-07 19:38:52 -0700

    Added projects page
    Projects html thanks to Olivia!

---
+++ projects.php
@@ -0,0 +1,47 @@
+<?php
+
+$pageTitle = "Foxtrot Designs | Projects"; //sets the page title
+$pageCss = "css/projects.css"; //sets page-specific CSS file
+$selectedNavId = "projects"; //options are "home", "projects", "news", "pricing", and "aboutus". This sets a class called "nav-selected" on the nav item.
+
+include "header.php";
+
+?>
+
+
+
+<p>Our expertise comes from <b>experience.</b> We have been designing web pages <b>since 2011.</b> Choosing an experienced wed design company like Foxtrot Designs ensures that your website will be professional and created in a timely manner.
+</p>
+
+<p>Browse some of our pervious projects to get a sample of our work.</p>
+
+<div class="row">
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>Hiking Website</h4>
+        <p>A website for posting trip logs and hiking articles.</p>
+    </div>
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>Strange Planes</h4>
+        <p>Responsive site documenting odd planes.</p>
+    </div>
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>Project 3</h4>
+        <p>Description...</p>
+    </div>
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>Project 4</h4>
+        <p>Description...</p>
+    </div>
+    <div class="large-4 columns">
+        <img src="http://placehold.it/400x300&text=[img]"/>
+        <h4>Project 5</h4>
+        <p>Description...</p>
+    </div>
+</div>
+
+
+<?php include "footer.php"; ?>
