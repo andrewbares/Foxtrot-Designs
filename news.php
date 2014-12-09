935f4816268d06151876f2f3094de208d505379c
andrewbares <andrewbares@outlook.com>
2014-12-08 22:22:51 -0700

    Added blank news.php and news.css files

---
+++ news.php
@@ -0,0 +1,17 @@
+<?php
+
+$pageTitle = "Foxtrot Designs | News"; //sets the page title
+$pageCss = "css/news.css"; //sets page-specific CSS file
+$selectedNavId = "news"; //options are "home", "projects", "news", "pricing", and "aboutus". This sets a class called "nav-selected" on the nav item.
+
+include "header.php";
+
+?>
+
+
+
+<!-- HTML body content goes here -->
+
+
+
+<?php include "footer.php"; ?>
