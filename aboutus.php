2e5e3794bb51f1b557d7e9b5da109025224cc462
andrewbares <andrewbares@outlook.com>
2014-12-10 09:22:26 -0700

    Added blank pricing and about us pages/css

---
+++ aboutus.php
@@ -0,0 +1,16 @@
+<?php
+
+$pageTitle = "Foxtrot Designs | About Us"; //sets the page title
+$pageCss = "css/aboutus.css"; //sets page-specific CSS file
+$selectedNavId = "aboutus"; //options are "home", "projects", "news", "pricing", and "aboutus". This sets a class called "nav-selected" on the nav item.
+
+include "header.php";
+
+?>
+
+
+<!-- HTML for about us goes here -->
+
+
+
+<?php include "footer.php"; ?>
---
+++ pricing.php
@@ -0,0 +1,16 @@
+<?php
+
+$pageTitle = "Foxtrot Designs | Pricing"; //sets the page title
+$pageCss = "css/pricing.css"; //sets page-specific CSS file
+$selectedNavId = "pricing"; //options are "home", "projects", "news", "pricing", and "aboutus". This sets a class called "nav-selected" on the nav item.
+
+include "header.php";
+
+?>
+
+
+<!-- HTML for pricing goes here -->
+
+
+
+<?php include "footer.php"; ?>
