caf4e9c7dab356483eaf83f98968e0b31c02bd10
andrewbares <andrewbares@outlook.com>
2014-12-12 13:45:25 -0700

    Projects - images and links added

--- projects.php
+++ projects.php
@@ -16,31 +16,46 @@ include "header.php";
 <p>Browse some of our pervious projects to get a sample of our work.</p>
 
 <div class="row">
-    <div class="large-4 columns">
-        <img src="http://placehold.it/400x300&text=[img]"/>
-        <h4>Hiking Website</h4>
-        <p>A website for posting trip logs and hiking articles.</p>
-    </div>
-    <div class="large-4 columns">
-        <img src="http://placehold.it/400x300&text=[img]"/>
-        <h4>Strange Planes</h4>
-        <p>Responsive site documenting odd planes.</p>
-    </div>
-    <div class="large-4 columns">
-        <img src="http://placehold.it/400x300&text=[img]"/>
-        <h4>Project 3</h4>
-        <p>Description...</p>
-    </div>
-    <div class="large-4 columns">
-        <img src="http://placehold.it/400x300&text=[img]"/>
-        <h4>Project 4</h4>
-        <p>Description...</p>
-    </div>
-    <div class="large-4 columns">
-        <img src="http://placehold.it/400x300&text=[img]"/>
-        <h4>Project 5</h4>
-        <p>Description...</p>
-    </div>
+    
+    <a href="http://hiking.andrewbares.net" target="_blank">
+        <div class="large-4 columns">
+            <img src="img/project1.jpg"/>
+            <h4>Hiking Website</h4>
+            <p>A website for posting trip logs and hiking articles.</p>
+        </div>
+    </a>
+    
+    <a href="http://u.arizona.edu/~andrewbares/etcv403/assignment9/" target="_blank">
+        <div class="large-4 columns">
+            <img src="img/project2.jpg"/>
+            <h4>Strange Planes</h4>
+            <p>Responsive site documenting odd planes.</p>
+        </div>
+    </a>
+    
+    <a href="http://gopaladhikari.com/" target="_blank">
+        <div class="large-4 columns">
+            <img src="img/project3.jpg"/>
+            <h4>Personal Blog</h4>
+            <p>A personal blog for Gopal, with the theme created by himself</p>
+        </div>
+    </a>
+    
+    <a href="http://apps.microsoft.com/windows/en-us/app/power-planner-homework-planner/7bb00b6b-7332-42e5-82a8-61da90418594" target="_blank">
+        <div class="large-4 columns">
+            <img src="img/project4.jpg"/>
+            <h4>Homework Planner for Windows 8.1</h4>
+            <p>A modern Windows 8.1 app that helps students manage their academic life.</p>
+        </div>
+    </a>
+    
+    <a href="http://www.windowsphone.com/en-us/store/app/8list-for-craigslist/efefb246-54cf-45d6-b039-492c3060f1ad" target="_blank">
+        <div class="large-4 columns">
+            <img src="img/project5.jpg"/>
+            <h4>Craigslist client for Windows Phone</h4>
+            <p>Browse craigslist seamlessly on your Windows Phone.</p>
+        </div>
+    </a>
 </div>
 
 
