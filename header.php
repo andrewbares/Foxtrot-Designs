fe1c34eb5762a8b67a150f057a2151dda78eba26
Gopal Adhikari <gopala@email.arizona.edu>
2014-12-13 16:31:25 -0700

    Added meta tag which allowed mobile browsers to switch to responsive CSS

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
     </title>
     
     <meta charset="utf-8" />
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--stylesheet for header and footer-->
     <link rel="stylesheet" type="text/css" href="css/global.css">
         
