52c501635ac60d8d65a2c335ffdd61cb547769ce
Gopal Adhikari <gopala@email.arizona.edu>
2014-12-12 20:20:36 -0700

    Fixed about page CSS for responsive display

--- aboutus.php
+++ aboutus.php
@@ -17,53 +17,54 @@ include "header.php";
 	<div class="person">
 		<h3>Gopal Adhikari</h3>
 		<img class="person-head" src="img/people/gopal.jpg">
-		<div class="person-description">
-			Tea enthusiast and all-round nerd, Gopal likes all things computers and hates writing biography in third person. Wait...
-		</div>
-		<div class="person-link"><a href="gopal.php">Gopal's Page</a></div>
+		<p>
+		Tea enthusiast and all-round nerd, Gopal likes all things computers and hates writing biography in third person. Wait...
+		<a href="gopal.php">Gopal's Page</a>
+		</p>
 	</div>
 
 	<!-- Andrew Bares -->
 	<div class="person">
 		<h3>Andrew Bares</h3>
 		<img class="person-head" src="http://placehold.it/150x150">
-		<div class="person-description">
+		<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor ante, euismod vel quam ut, mattis blandit turpis. Donec nec ex sit amet ex pulvinar 
 		gravida dapibus sit amet quam. Aenean tempus velit velit, et laoreet felis mattis et. Nulla finibus sed turpis ac sodales. Praesent sit amet risus nisl.
-		</div>
-		<div class="person-link"><a href="andrew.php">Andrew's Page</a></div>
+		<a href="andrew.php">Andrew's Page</a>
+		</p>
 	</div>
 	
 	<!-- Olivia Goldson -->
 	<div class="person">
 		<h3>Olivia Goldson</h3>
 		<img class="person-head" src="http://placehold.it/150x150">
-		<div class="person-description">
+		<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor ante, euismod vel quam ut, mattis blandit turpis. Donec nec ex sit amet ex pulvinar 
-		gravida dapibus sit amet quam. Aenean tempus velit velit, et laoreet felis mattis et. Nulla finibus sed turpis ac sodales. Praesent sit amet risus nisl.		</div>
-		<div class="person-link"><a href="olivia.php">Olivia's Page</a></div>
+		gravida dapibus sit amet quam. Aenean tempus velit velit, et laoreet felis mattis et. Nulla finibus sed turpis ac sodales. Praesent sit amet risus nisl.
+		<a href="olivia.php">Olivia's Page</a>
+		</p>
 	</div>
 
 	<!-- Aaron Stewart -->
 	<div class="person">
 		<h3>Aaron Stewart</h3>
 		<img class="person-head" src="http://placehold.it/150x150">
-		<div class="person-description">
+		<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor ante, euismod vel quam ut, mattis blandit turpis. Donec nec ex sit amet ex pulvinar 
 		gravida dapibus sit amet quam. Aenean tempus velit velit, et laoreet felis mattis et. Nulla finibus sed turpis ac sodales. Praesent sit amet risus nisl.
-		</div>
-		<div class="person-link"><a href="aaron.php">Aaron's Page</a></div>
+		<a href="aaron.php">Aaron's Page</a>
+		</p>
 	</div>
 	
 	<!-- Jeannie Leonard -->
 	<div class="person">
 		<h3>Jeannie	Leonard</h3>
 		<img class="person-head" src="http://placehold.it/150x150">
-		<div class="person-description">
+		<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor ante, euismod vel quam ut, mattis blandit turpis. Donec nec ex sit amet ex pulvinar 
 		gravida dapibus sit amet quam. Aenean tempus velit velit, et laoreet felis mattis et. Nulla finibus sed turpis ac sodales. Praesent sit amet risus nisl.
-		</div>
-		<div class="person-link"><a href="jeannie.php">Jeannie's Page</a></div>
+		<a href="jeannie.php">Jeannie's Page</a>
+		</p>
 	</div>
 	
 	
