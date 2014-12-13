e246ded3693262c83cab86c15a9dcb246fa2dd46
Gopal Adhikari <gopala@email.arizona.edu>
2014-12-13 15:40:23 -0700

    Added Olivia's pricing HTML and CSS

--- pricing.php
+++ pricing.php
@@ -8,9 +8,42 @@ include "header.php";
 
 ?>
 
-
 <!-- HTML for pricing goes here -->
 
+<div class="pricing-table-container" data-min-col-width="300">
 
+	<p>The following are <b>estimates</b> of what our typical website cost. For a quote specific to your own website, please contact us.</p>
+
+
+	<div class="pricing-table">
+		<ul>
+			<li class="heading">Basic Site</li>
+			<li class="price">$500</li>
+			<li>Starter package</li>
+			<li>A typical site, like this current one. Consists of a few static pages.</li>
+			<li class="action">Sign up now!</li>
+		</ul>
+
+		<ul class="feature">
+			<li class="heading">Average Site</li>
+			<li class="price">$1,000</li>
+			<li>Intermediate package</li>
+			<li>A basic site+ some additional advanced functionality like dynamic pages, a user admin center, or simply more content.</li>
+			<li class="action">Sign up now!</li>
+		</ul>
+
+		<ul>
+			<li class="heading">Advanced site</li>
+			<li class="price">$3,000</li>
+			<li>Professional package</li>
+			<li>A full-scale website with the dynamic content, user admin center and numerous pages of content.</li>
+			<li class="action">Sign up now!</li>
+		</ul>
+
+		<div class="clearfix"></div>
+
+	</div>
+
+</div>
 
 <?php include "footer.php"; ?>
