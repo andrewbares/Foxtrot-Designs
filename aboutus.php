b69bb25d63beb9d469f0e00fe587225d2ab87012
Gopal Adhikari <gopala@email.arizona.edu>
2014-12-12 18:54:18 -0700

    Added about page and stylesheet for it

--- aboutus.php
+++ aboutus.php
@@ -8,9 +8,23 @@ include "header.php";
 
 ?>
 
-
 <!-- HTML for about us goes here -->
-
-
+<section class="about">
+	<h2>About Foxtrot Design</h2>
+	<p>
+		We had lots of fun designing this website. Even though we are a fictitious company, we have realized that
+		having a website is very important for any business. If you own a business it's important to maintain an online
+		presence, and having a website for that business is a crucial step forward in keeping your potential
+		customers informed about the services you offer.
+	</p>
+	<p>
+		Working on a team project remotely is a daunting task, but each of us finished the task we were
+		given in a timely manner. We were also given option to be creative with doing ofur respective tasks.
+	</p>
+	<p>
+		Overall it was a great a learning experience. We learned a lot about writing responsive CSS and semantic HTML markup.
+		We are all very excited about the future of web design, and what it has to offer.
+	</p>
+</section>
 
 <?php include "footer.php"; ?>
